a91d3f0ba312253cb9269fab8fafdee222fc5106
Aarthi16-dev <annaduraiaarthi8@gmail.com>
2026-04-10 21:54:58 +0530

    Final CORS consolidation: Consolidate configuration and fix 401 on /me

--- backend/src/main/java/com/blooddrop/controller/AuthenticationController.java
+++ backend/src/main/java/com/blooddrop/controller/AuthenticationController.java
@@ -11,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
-@CrossOrigin(originPatterns = "*", allowCredentials = "true")
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
